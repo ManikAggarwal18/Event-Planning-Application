21366ec5a4ea7dc2f2325f52f9a23a6dcdd35cd8
Manik <manikaggarwal.2003@gmail.com>
2024-04-20 18:30:55 +0530

    first commit

---
+++ FRONTEND/src/components/About.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="about container">
+      <h4>Events</h4>
+      <h2>ABOUT</h2>
+      <p>
+      Welcome to MANIK's Events & Weddings! We're passionate about creating unforgettable moments for birthdays, anniversaries, 
+      camping trips, game nights, parties, and weddings. Our dedicated team works closely with you, 
+      ensuring every detail reflects your unique vision. With years of experience and a vast network of trusted vendors, 
+      we make planning stress-free and ensure your event exceeds expectations. From intimate gatherings to grand celebrations, 
+      we're here to bring your dreams to life with creativity and precision. Let us be your partner in celebration, guiding 
+      you to unforgettable memories that will last a lifetime. Contact us today to begin planning your special event with MANIK's Events & Weddings!.
+      </p>
+    </section>
+  );
+};
+
+export default About;
